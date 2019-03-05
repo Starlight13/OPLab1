652fb7619b3b848805143f3ab26bcbfea01f89d9
dsadas asdasd <olgarom@macbook-pro.local>
2019-03-05 19:39:29 +0200

    Edit func

--- OPLab1/Func.cpp
+++ OPLab1/Func.cpp
@@ -4,20 +4,23 @@
 #include <iomanip>
 using namespace std;
 
-void budget(students *array){
-    for(int i = 0; i < 5; i++){
+int budget(students *array, int numberOfStudents ){
+    int notContract = 0;
+    for(int i = 0; i < numberOfStudents; i++){
         if(array[i].isContract == false){
             array[i].avg = (array[i].g1+array[i].g2+array[i].g3+array[i].g4+array[i].g5)/5;
+            notContract += 1;
         }
         else{
             array[i].avg = 0;
         }
     }
+    return notContract;
 }
 
-void sort(students *array){
+void sort(students *array, int numberOfStudents){
     students temp;
-    for (int i = 1; i < 5; i++){
+    for (int i = 1; i < numberOfStudents; i++){
         temp = array[i];
         int j = i-1;
         while (j >= 0 && array[j].avg < temp.avg) {
@@ -28,10 +31,10 @@ void sort(students *array){
     }
 }
 
-void scholarship(students *array){
-    int num = 5*0.4;
+void scholarship(students *array, int notContracts){
+    int num = notContracts*0.4;
     for(int i = 0; i < num; i++){
         cout << array[i].name << " " << array[i].g1 << " " << array[i].g2 << " " << array[i].g3 << " " << array[i].g4 << " " << array[i].g5 << " Avarage: " << array[i].avg << endl;
     }
-    cout << array[num-1].avg;
+    cout << "Minimum avarage: " << array[num-1].avg;
 }
