f2586c2eddedbce3f09e76cb0f0ed9305f3b07bb
dsadas asdasd <olgarom@macbook-pro.local>
2019-03-05 12:31:49 +0200

    Files

---
+++ OPLab1/File.cpp
@@ -0,0 +1,9 @@
+//
+//  File.cpp
+//  OPLab1
+//
+//  Created by dsadas asdasd on 3/5/19.
+//  Copyright © 2019 dsadas asdasd. All rights reserved.
+//
+
+#include <stdio.h>
--- OPLab1/main.cpp
+++ OPLab1/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  OPLab1
-//
-//  Created by dsadas asdasd on 3/5/19.
-//  Copyright © 2019 dsadas asdasd. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
