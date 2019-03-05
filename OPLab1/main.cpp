641107c9f68d2391166bdfe8f5ce05a1a6e845ec
dsadas asdasd <olgarom@macbook-pro.local>
2019-03-05 12:21:30 +0200

    Initial Commit

---
+++ OPLab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OPLab1
+//
+//  Created by dsadas asdasd on 3/5/19.
+//  Copyright © 2019 dsadas asdasd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
